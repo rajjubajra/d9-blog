8423520a9db22f89e4469a99e7c4bfafc015a718
rajju <vajrachara@yahoo.com>
2021-06-03 16:34:06 +0100

    updates

--- b5_01_blog/src/Config/dataurl.js
+++
@@ -1,9 +0,0 @@
-
-export const baseUrl = {
-  URL: `https://www.yellow-website.com/d9-blog`,
-}
-
-
-export const photourl = {
-  PHOTOURL: 'https://yellow-website.com/d8-react-base-theme-backend/photo-placeholder?_format=json'
-}
