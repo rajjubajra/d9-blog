daa46dd579438479f0a44041140c22a5b0722979
rajju <vajrachara@yahoo.com>
2021-06-25 10:35:03 +0100

    working on tw2_01_blog

--- tw2_01_blog/src/App.js
+++ tw2_01_blog/src/App.js
@@ -1,13 +1,20 @@
 import {Provider} from 'react-redux';
 import {store} from './Redux/store';
 import Router from './Router';
+import MenuBar from './componensts/MainMenu/MenuBar';
+import {BrowserRouter} from 'react-router-dom';
 
 
 function App() {
 
   return (
     <Provider store={store}>
-      <Router />
+      <BrowserRouter>
+      <div className="bg-gray-100 min-h-full w-full">
+        <MenuBar />
+        <Router />
+      </div>
+      </BrowserRouter>
     </Provider>
   );
 }
--- tw2_01_blog/src/Router.js
+++ tw2_01_blog/src/Router.js
@@ -1,9 +1,9 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 import Home from './Page/Home';
 import About from './Page/About';
 import Contact from './Page/Contact';
 import Blog from './Page/Blog';
-import MenuBar from './componensts/MainMenu/MenuBar';
+
 import BlogPost from './Page/BlogPost';
 import BlogListByTaxonomy from './Page/BlogListByTaxonomy';
 import BlogSearch from './Page/BlogSearch';
@@ -12,14 +12,8 @@ import BlogSearch from './Page/BlogSearch';
 
 function Router() {
 
-
-
   return (
-    <div className="bg-gray-100 min-h-full w-full">
-      <BrowserRouter>
-        <MenuBar />
       <Switch>
-        <Route path="/d9-blog/react-js/tw2-01/" component={Home} />
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
@@ -28,8 +22,6 @@ function Router() {
         <Route path="/blogs-by-tag/:tid" component={BlogListByTaxonomy} />
         <Route path="/blog-search" component={BlogSearch} />
       </Switch>
-      </BrowserRouter>
-    </div>
   )
 }
 
