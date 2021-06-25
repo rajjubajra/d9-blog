0bf20c5ad45edcae4c530db66cec3f9a5accf163
rajju <vajrachara@yahoo.com>
2021-06-25 16:08:13 +0100

    working on tw2_01_blog

--- tw2_01_blog/src/App.js
+++ tw2_01_blog/src/App.js
@@ -2,7 +2,7 @@ import {Provider} from 'react-redux';
 import {store} from './Redux/store';
 import Router from './Router';
 import MenuBar from './componensts/MainMenu/MenuBar';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Link} from 'react-router-dom';
 
 
 function App() {
@@ -12,6 +12,9 @@ function App() {
       <BrowserRouter>
       <div className="bg-gray-100 min-h-full w-full">
         <MenuBar />
+        <div>
+          <Link to="/">View Page</Link>
+        </div>
         <Router />
       </div>
       </BrowserRouter>
