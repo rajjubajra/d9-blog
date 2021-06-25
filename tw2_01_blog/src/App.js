49fb197b09a9835c60d277b04c4524f1f029c6d5
rajju <vajrachara@yahoo.com>
2021-06-25 16:22:55 +0100

    working on tw2_01_blog

--- tw2_01_blog/src/App.js
+++ tw2_01_blog/src/App.js
@@ -3,17 +3,23 @@ import {store} from './Redux/store';
 import Router from './Router';
 import MenuBar from './componensts/MainMenu/MenuBar';
 import {BrowserRouter, Link} from 'react-router-dom';
+import {useState} from 'react';
 
 
 function App() {
+
+  const [state, setState] = useState(true);
 
   return (
     <Provider store={store}>
       <BrowserRouter>
       <div className="bg-gray-100 min-h-full w-full">
         <MenuBar />
-        <div>
-          <Link to="/">View Page</Link>
+        <div className={`w-full min-h-screen flex justify-center items-center 
+            ${state ? '' : 'hidden'}`}
+            onClick={()=>setState(false)}
+          >
+          <Link  to="/">Live Preview</Link>
         </div>
         <Router />
       </div>
--- tw2_01_blog/src/Router.js
+++ tw2_01_blog/src/Router.js
@@ -14,7 +14,7 @@ function Router() {
 
   return (
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/blog" component={Blog} />
