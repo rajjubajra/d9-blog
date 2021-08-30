39ecece4d9e20e5007808d347af7e91e925ad830
rajjubajra <rajjubajra@gmail.com>
2021-08-30 20:27:43 +0100

    router.js file updated

--- tw2-01-blog/src/App.js
+++ tw2-01-blog/src/App.js
@@ -2,25 +2,16 @@ import {Provider} from 'react-redux';
 import {store} from './Redux/store';
 import Router from './Router';
 import MenuBar from './componensts/MainMenu/MenuBar';
-import {BrowserRouter, Link} from 'react-router-dom';
-import {useState} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
 
 function App() {
-
-  const [state, setState] = useState(true);
 
   return (
     <Provider store={store}>
       <BrowserRouter>
       <div className="bg-gray-100 min-h-full w-full">
         <MenuBar />
-        <div className={`w-full min-h-screen flex justify-center items-center 
-            ${state ? '' : 'hidden'}`}
-            onClick={()=>setState(false)}
-          >
-          <Link  to="/">Live Preview</Link>
-        </div>
         <Router />
       </div>
       </BrowserRouter>
--- tw2-01-blog/src/Router.js
+++ tw2-01-blog/src/Router.js
@@ -12,15 +12,17 @@ import BlogSearch from './Page/BlogSearch';
 
 function Router() {
 
+  const baseurl = "/d9-blog/themes/tw2-01";
+
   return (
       <Switch>
-        <Route exact path="/d9-blog/themes/tw2-01" component={Home} />
-        <Route path="/d9-blog/themes/tw2-01/about" component={About} />
-        <Route path="/d9-blog/themes/tw2-01/contact" component={Contact} />
-        <Route path="/d9-blog/themes/tw2-01/blog" component={Blog} />
-        <Route path="/d9-blog/themes/tw2-01/blog-post/:nid" component={BlogPost} />
-        <Route path="/d9-blog/themes/tw2-01/blogs-by-tag/:tid" component={BlogListByTaxonomy} />
-        <Route path="/d9-blog/themes/tw2-01/blog-search" component={BlogSearch} />
+        <Route exact path={`${baseurl}`}  component={Home} />
+        <Route path={`${baseurl}/about`}  component={About} />
+        <Route path={`${baseurl}/contact`} component={Contact} />
+        <Route path={`${baseurl}/blog`}component={Blog} />
+        <Route path={`${baseurl}/blog-post/:nid`} component={BlogPost} />
+        <Route path={`${baseurl}/blogs-by-tag/:tid`} component={BlogListByTaxonomy} />
+        <Route path={`${baseurl}/blog-search`} component={BlogSearch} />
       </Switch>
   )
 }
