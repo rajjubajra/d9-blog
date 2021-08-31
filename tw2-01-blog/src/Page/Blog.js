e550b087fcd87ad38729addbe6c7d4e72f0ec900
rajjubajra <rajjubajra@gmail.com>
2021-08-31 07:19:46 +0100

    work on BlogCard

--- tw2-01-blog/src/Page/Blog.js
+++ tw2-01-blog/src/Page/Blog.js
@@ -1,8 +1,11 @@
+import {useEffect} from 'react';
+
 import BlogListTwo from './Blog/BlogListTwo';
 import Tags from '../componensts/Taxonomy/Tags';
 import FormSelectDate from './Blog/FormSelectDate';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import BlogListOfTheMonth from './Blog/BlogListOfTheMonth';
+import {actionAll} from '../Redux/Action';
 
 function Blog() {
   
@@ -10,6 +13,12 @@ function Blog() {
   const data = useSelector(state=>state.reducerBlog.blog_data);
 
   console.log("data",data);
+  const dispatch = useDispatch();
+
+useEffect(()=>{
+  dispatch(actionAll());
+},[dispatch])
+
 
   return (
     <div className="m-10">
